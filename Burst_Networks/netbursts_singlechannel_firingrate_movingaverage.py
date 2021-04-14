8407c3e3b0238c98f98bfcb06d16a37c94c0cb94
Jonas Ort <jonas.ort@protonmail.com>
2021-04-14 16:19:06 +0200

    To Do for Networkburst detections and MEA Plot template.

---
+++ Burst_Networks/netbursts_singlechannel_firingrate_movingaverage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 14 10:17:44 2021
+
+@author: jonas
+"""
+
+
+input = raw signal aus .h5 data,
+        spiketimes aus dem main script (spikedic)
+        burststartdic
+
+- > Signal ist ein array aus floats, diese geben die voltage an
+
---
+++ Burst_Networks/untitled0.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 14 10:17:44 2021
+
+@author: jonas
+"""
+
+
